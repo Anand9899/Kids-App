6f636c6b553532aa2e125c33bbcad5104891ccc1
Anand Kumar Mishra <125265758+Anand9899@users.noreply.github.com>
2024-02-13 12:24:23 +0530

    my first web hosting

---
+++ JS/index.js
@@ -0,0 +1,227 @@
+function speak(letter) {
+    var audio = document.getElementById("audio");
+    var speaker = document.getElementById("speaker");
+    var result = document.getElementById("result");
+    var img = document.getElementById("img");
+
+    if(letter == 'a')
+    {
+        audio.src = "Sound/Apple.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Apple";
+        img.style.display = "block";
+        img.src = "Image/Apple.jpeg";
+    }
+    else if(letter == 'b')
+    {
+        audio.src = "Sound/Ball.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Ball";
+        img.style.display = "block";
+        img.src = "Image/Ball.webp";
+    }
+    else if(letter == 'c')
+    {
+        audio.src = "Sound/Cat.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Cat";
+        img.style.display = "block";
+        img.src = "Image/Cat.jpg";
+    }
+    else if(letter == 'd')
+    {
+        audio.src = "Sound/Dog.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Dog";
+        img.style.display = "block";
+        img.src = "Image/Dog.jpg";
+    }
+    else if(letter == 'e')
+    {
+        audio.src = "Sound/Elephant.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Elephant";
+        img.style.display = "block";
+        img.src = "Image/Elephant.jpg";
+    }
+    else if(letter == 'f')
+    {
+        audio.src = "Sound/Fish.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Fish";
+        img.style.display = "block";
+        img.src = "Image/Fish.jpg";
+    }
+    else if(letter == 'g')
+    {
+        audio.src = "Sound/Gun.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Gun";
+        img.style.display = "block";
+        img.src = "Image/Gun.jpg";
+    }
+    else if(letter == 'h')
+    {
+        audio.src = "Sound/Hen.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Hen";
+        img.style.display = "block";
+        img.src = "Image/Hen.jpg";
+    }
+    else if(letter == 'i')
+    {
+        audio.src = "Sound/Ice-Cream.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Ice-Cream";
+        img.style.display = "block";
+        img.src = "Image/Ice-Cream.jpg";
+    }
+    else if(letter == 'j')
+    {
+        audio.src = "Sound/Jug.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Jug";
+        img.style.display = "block";
+        img.src = "Image/Jug.webp";
+    }
+    else if(letter == 'k')
+    {
+        audio.src = "Sound/Kite.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Kite";
+        img.style.display = "block";
+        img.src = "Image/Kite.webp";
+    }
+    else if(letter == 'l')
+    {
+        audio.src = "Sound/Lion.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Lion";
+        img.style.display = "block";
+        img.src = "Image/Lion.webp";
+    }
+    else if(letter == 'm')
+    {
+        audio.src = "Sound/Mango.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Mango";
+        img.style.display = "block";
+        img.src = "Image/Mango.webp";
+    }
+    else if(letter == 'n')
+    {
+        audio.src = "Sound/Nest.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Nest";
+        img.style.display = "block";
+        img.src = "Image/Nest.webp";
+    }
+    else if(letter == 'o')
+    {
+        audio.src = "Sound/Orange.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Orange";
+        img.style.display = "block";
+        img.src = "Image/Orange.jpg";
+    }
+    else if(letter == 'p')
+    {
+        audio.src = "Sound/Parrot.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Parrot";
+        img.style.display = "block";
+        img.src = "Image/Parrot.jpg";
+    }
+    else if(letter == 'q')
+    {
+        audio.src = "Sound/Queen.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Queen";
+        img.style.display = "block";
+        img.src = "Image/Queen.webp";
+    }
+    else if(letter == 'r')
+    {
+        audio.src = "Sound/Rose.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Rose";
+        img.style.display = "block";
+        img.src = "Image/Rose.webp";
+    }
+    else if(letter == 's')
+    {
+        audio.src = "Sound/Sun.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Sun";
+        img.style.display = "block";
+        img.src = "Image/Sun.avif";
+    }
+    else if(letter == 't')
+    {
+        audio.src = "Sound/Tiger.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Tiger";
+        img.style.display = "block";
+        img.src = "Image/Tiger.jpg";
+    }
+    else if(letter == 'u')
+    {
+        audio.src = "Sound/Umbrella.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Umbrella";
+        img.style.display = "block";
+        img.src = "Image/Umbrella.jpg";
+    }
+    else if(letter == 'v')
+    {
+        audio.src = "Sound/Van.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Van";
+        img.style.display = "block";
+        img.src = "Image/Van.jpg";
+    }
+    else if(letter == 'w')
+    {
+        audio.src = "Sound/Watch.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Watch";
+        img.style.display = "block";
+        img.src = "Image/Watch.avif";
+    }
+    else if(letter == 'x')
+    {
+        audio.src = "Sound/X-ray.mp3";
+        result.style.display = "block"
+        result.innerHTML = "X-Ray";
+        img.style.display = "block";
+        img.src = "Image/X-Ray.jpg";
+    }
+    else if(letter == 'y')
+    {
+        audio.src = "Sound/Yak.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Yak";
+        img.style.display = "block";
+        img.src = "Image/Yak.jpg";
+    }
+    else if(letter == 'z')
+    {
+        audio.src = "Sound/Zebra.mp3";
+        result.style.display = "block"
+        result.innerHTML = "Zebra";
+        img.style.display = "block";
+        img.src = "Image/Zebra.webp";
+    }
+    else alert("Not found");
+
+    speaker.style.color = "black";
+    audio.play();
+
+    setTimeout(function(){
+        speaker.style.color = "#ddd";
+        result.innerHTML = "";
+        result.style.display = "none";
+        img.innerHTMLn = "";
+        img.style.display = "none";
+    }, 1000);
+}
